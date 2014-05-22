e55702a9c690298faf2279532dc1ee08eb02de4d
Dmitriy Kataskin <dmitriy.kataskin@gmail.com>
2014-05-21 23:32:13 -0400

    added webix

--- example/primitive/controllers/default.js
+++ example/primitive/controllers/default.js
@@ -1,9 +1,15 @@
 exports.install = function(framework) {
     framework.route('/', view_login);
+    framework.route('/chat', view_chat);
 };
  
 // login view
 function view_login() {
     var self = this;
     self.view('login');
+}
+
+function view_chat() {
+    var self = this;
+    self.view('chat');
 }
