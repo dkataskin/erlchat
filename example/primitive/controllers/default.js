00d1ed2df7808e71b4e1d2af9ef7596f45fa7201
Dmitriy Kataskin <dmitriy.kataskin@gmail.com>
2014-05-22 21:44:50 -0400

    example: authorization

--- example/primitive/controllers/default.js
+++ example/primitive/controllers/default.js
@@ -1,6 +1,25 @@
 exports.install = function(framework) {
-    framework.route('/', view_login);
-    framework.route('/chat', view_chat);
+
+    framework.onAuthorization = function(req, res, flags, next) {
+        var userId = req.cookie('user').parseInt();
+
+        if (userId === 0) {
+            // unlogged user
+            next(false);
+            return;
+        };
+
+        next(true, {});
+    };
+
+    framework.route('/', redirect_login);
+    framework.route('/', redirect_chat, ['authorize']);
+
+    framework.route('/login', view_login);
+    framework.route('/login', post_login, ['post', 'xhr']);
+
+    framework.route('/chat', redirect_login);
+    framework.route('/chat', view_chat, ['authorize']);
 };
  
 // login view
@@ -9,7 +28,26 @@ function view_login() {
     self.view('login');
 }
 
+// chat view
 function view_chat() {
     var self = this;
     self.view('chat');
 }
+
+function post_login() {
+    var self = this;
+
+    self.redirect('/chat');
+}
+
+function redirect_login(){
+    var self = this;
+
+    self.redirect('/login');
+};
+
+function redirect_chat(){
+    var self = this;
+
+    self.redirect('/login');
+};
