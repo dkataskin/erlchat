8ad996b9cac604e9084b05f7c07eb5ccb78695ca
Dmitriy Kataskin <dmitriy.kataskin@gmail.com>
2014-05-26 22:25:09 -0400

    sample: shaping layout

--- example/primitive/public/js/chat.js
+++ example/primitive/public/js/chat.js
@@ -5,23 +5,31 @@
             { cols:[
                  { type:"clean" },
                  { width:300, type:"clean", rows:[
-                    { view:"template", height:50 },
-                    { view:"template" }
+                    { view:"template", type:"line", height:50 },
+                    { view:"template", type:"line" }
                  ]},
                  { width:700, rows:[
-                       { view:"layout", type:"line", rows:[
-                               { template:"<span style='font-size:16px; text-align:center; font-weight:bold'>John Keats\' Verses</span>", height:50},
-                               { view:"scrollview", id:"verses", scroll:"y",
-                                   body:{
-                                       rows:[
-                                           { template:"<i>Scroll down for the next verse</i>", height:30, type:"clean" },
-                                           { template:"<i>Scroll down for the next verse</i>", height:30, type:"clean" },
-                                           { template:"<i>Scroll down for the next verse</i>", height:30, type:"clean" },
-                                       ]
-                                   } //end of scrollview body
-                               }
-                           ]
-                       }]
+                       { view:"template", type:"line", height:50 },
+                       { view:"scrollview", scroll:"y",
+                              body:{
+                                  rows:[
+                                      { template:"<i>Scroll down for the next verse</i>", height:30, type:"clean" },
+                                      { template:"<i>Scroll down for the next verse</i>", height:30, type:"clean" },
+                                      { template:"<i>Scroll down for the next verse</i>", height:30, type:"clean" },
+                                  ]
+                              } //end of scrollview body
+                          },
+                       { type:"clean", rows:[
+                            { type:"clean", height:70, cols:[
+                                { type:"clean", width:70 },
+                                { view:"textarea", placeholder:"Write a message...", height:70 },
+                                { type:"clean", width:70 }
+                            ]},
+                            { type:"clean", height:35, cols:[
+                                { type:"clean", width:70 },
+                                { view:"button", value:"Send", inputWidth:100 }
+                            ]},
+                       ]}]
                  },
                  { type:"clean" }
             ]},
