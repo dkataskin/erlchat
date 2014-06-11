47d0a239a865f780e9f05d30a73af0d3395a3384
Dmitriy Kataskin <dmitriy.kataskin@gmail.com>
2014-06-10 23:32:53 -0400

    uuid generator

--- src/erlchat_utils.erl
+++ src/erlchat_utils.erl
@@ -30,12 +30,33 @@
 -author("Dmitry Kataskin").
 
 %% API
--export([get_unique_id/0, priv_dir/0]).
+-export([get_unique_id/0, priv_dir/0, generate_uuid/0]).
 
 get_unique_id() ->
-                {M, S, U} = erlang:now(),
-                <<N:64>> = <<M:24, S:20, U:20>>,
-                N.
+        {M, S, U} = erlang:now(),
+        <<N:64>> = <<M:24, S:20, U:20>>,
+        N.
+
+generate_uuid() ->
+        Now = {_, _, Micro} = now(),
+        Nowish = calendar:now_to_universal_time(Now),
+        Nowsecs = calendar:datetime_to_gregorian_seconds(Nowish),
+        Then = calendar:datetime_to_gregorian_seconds({{1970, 1, 1}, {0, 0, 0}}),
+        Prefix = io_lib:format("~14.16.0b", [(Nowsecs - Then) * 1000000 + Micro]),
+        list_to_binary(Prefix ++ to_hex(crypto:rand_bytes(9))).
+
+to_hex([]) ->
+        [];
+
+to_hex(Bin) when is_binary(Bin) ->
+        to_hex(binary_to_list(Bin));
+to_hex([H|T]) ->
+        [to_digit(H div 16), to_digit(H rem 16) | to_hex(T)].
+
+to_digit(N) when N < 10 ->
+        $0 + N;
+to_digit(N) ->
+        $a + N-10.
 
 priv_dir() ->
         Ebin = filename:dirname(code:which(erlchat)),
